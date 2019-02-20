eb31d02aaca184997eea185fe05ff01428a93e99
eymen alagöz <bunyamineymen@gmail.com>
2019-02-20 15:45:42 +0300

    qqq77

---
+++ LineRendererEdgeCollider/Assets/Scripts/DrawLine.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+
+    public GameObject linePrefab;
+    public GameObject currentLine;
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D edgeCollider;
+    public List<Vector2> fingerPositions;
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CreateLine();
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 InputPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 tempFingerPos = new Vector2(InputPosition.x, InputPosition.y);
+
+            if (Vector2.Distance(tempFingerPos , fingerPositions[fingerPositions.Count - 1]) > .1f)
+            {
+                UpdateLine(tempFingerPos);
+            }
+        }
+    }
+
+    void CreateLine()
+    {
+        currentLine = Instantiate(linePrefab , Vector3.zero , Quaternion.identity);
+        lineRenderer = currentLine.GetComponent<LineRenderer>();
+        edgeCollider = currentLine.GetComponent<EdgeCollider2D>();
+        fingerPositions.Clear();
+
+
+
+        
+
+
+        Vector3 InputPositionBefore = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 tempFingerPosBefore = new Vector2(InputPositionBefore.x, InputPositionBefore.y);
+
+        Vector3 InputPositionAfter = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 tempFingerPosAfter = new Vector2(InputPositionAfter.x, InputPositionAfter.y);
+
+        fingerPositions.Add(Camera.main.ScreenToWorldPoint(tempFingerPosBefore));
+        fingerPositions.Add(Camera.main.ScreenToWorldPoint(tempFingerPosAfter));
+
+        lineRenderer.SetPosition(0,fingerPositions[0]);
+        lineRenderer.SetPosition(1, fingerPositions[1]);
+
+        edgeCollider.points = fingerPositions.ToArray();
+
+    }
+
+    void UpdateLine(Vector2 newFingerPos)
+    {
+        fingerPositions.Add(newFingerPos);
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(lineRenderer.positionCount - 1 , newFingerPos);
+        edgeCollider.points = fingerPositions.ToArray();
+
+    }
+
+}
